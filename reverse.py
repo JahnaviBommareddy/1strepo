6b72e8615950d36186e01d66d60ed65c2ccde8e7
JahnaviBommareddy <bommareddylakshmijahnavi@gmail.com>
2026-01-28 16:07:37 +0530

    reverse is added

---
+++ reverse.py
@@ -0,0 +1,15 @@
+'''write a program to fing the reverse of the given number'''
+def reverse(n):
+    rev=0
+    while n>0:
+        rev=rev*10+n%10
+        n//=10
+    return rev
+
+def ispalindrome(n):
+    return n==reverse(n)
+
+print(reverse(123))
+print(ispalindrome(123))
+print(reverse(121))
+print(ispalindrome(121))
