0183d7733df5ac4df971d24a20fdf7c43c5d5616
JahnaviBommareddy <bommareddylakshmijahnavi@gmail.com>
2026-01-28 16:56:29 +0530

    reverse added

--- reverse.py
+++ reverse.py
@@ -13,3 +13,11 @@ print(reverse(123))
 print(ispalindrome(123))
 print(reverse(121))
 print(ispalindrome(121))
+
+def getPalindromes(start,end):
+    res=""
+    for i in range(1,end+1):
+        if ispalindrome(i):
+            res=res+str(i)+","
+    return res
+print(getPalindromes(1,10000))
